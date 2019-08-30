54f7620e144f177446c06759b85c2aabb9984178
unknown <sbd12b@gmail.com>
2019-08-30 15:02:49 -0400

    restructure bs codes

--- wordpress-api/src/components/Posts.js
+++ wordpress-api/src/components/Posts.js
@@ -3,15 +3,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 const Posts = ({posts}) => {
     return (
-        <div>
+        <div className="py-5">
         <center><h1>Contact List</h1></center>
-        <Row>
+        <Row className="mt-5">
         {posts.map((post) => (
         <Col md={6} className="mt-3 mt-md-0">
             <div className="card">
                 <div className="card-body">
-                <h5 className="card-title">{post.title.rendered}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{post.content.rendered}</h6>
+                    <img alt={post.title.rendered} className="w-100" src={post.acf.post_image} />
+                    <h5 className="mt-3 card-title">{post.title.rendered}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted"><p dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}></p></h6>
                 </div>
             </div>
         </Col>
